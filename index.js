d3391e9c8ec9b252fdaa58a688837d615ef656f3
heidiwu3388 <heidiwu3388@yahoo.com>
2022-11-18 23:50:34 -0800

    try with first question

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the project?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (error) =>
+  error ? console.error(err) : console.log(`Wrote to ${fileName} successfully!`)
+);
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const readMeString = generateMarkdown(answers);
+    writeToFile("./output/README.md", readMeString);
+  })
+}
 
 // Function call to initialize app
 init();
--- output/README.md
+++ output/README.md
@@ -1,22 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  let badge = "badge";
-  return badge;
-}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  const template =  `
-    # ${data.title}
+    # testing
 
     ## Description
 
@@ -86,9 +69,4 @@ function generateMarkdown(data) {
 
     ## License
     This project is licensed under the terms of the MIT license.
-  `;
-
-  return template;
-}
-
-module.exports = generateMarkdown;
+  
